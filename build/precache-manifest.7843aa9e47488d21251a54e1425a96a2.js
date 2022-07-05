1ecd9f054a16b11503567b3f0e46ba3a326f650e
Hunter Norris <hunter.t.norris@gmail.com>
2022-07-05 15:03:57 -0500

    Class project about react router

---
+++ build/precache-manifest.7843aa9e47488d21251a54e1425a96a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be00e65e1f797d7a387c52f6ad566a25",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1486446f8ef00abe17a",
+    "url": "/static/css/main.c3a311c1.chunk.css"
+  },
+  {
+    "revision": "0392a618e55a6705606d",
+    "url": "/static/js/2.e5615630.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.e5615630.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1486446f8ef00abe17a",
+    "url": "/static/js/main.d4daac00.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.d4daac00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c56e041857a0bf600d3",
+    "url": "/static/js/runtime-main.3715f0a4.js"
+  }
+]);
